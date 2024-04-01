78556cce4658bb44ff51202523534a3e560fd316
advik-holalu <advikholalu@gmail.com>
2024-04-01 23:30:23 +0530

    first commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Adviks Hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AdvikHYP"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "ARH"
 )
 
 var ID ids.ID
